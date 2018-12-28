2a6ae08fd51d6ab6611b6503d6d9189ae1f2afc1
Andrew Camps <andrewcamps@Andrews-MacBook-Pro.local>
2018-12-28 00:27:35 -0800

    message query working

--- src/routes.js
+++ src/routes.js
@@ -3,13 +3,19 @@ const {google} = require('googleapis');
 const path = require('path');
 const fs = require('fs');
 const {server} = require('./config');
+const spending = require('./spending');
 
-router.use('/gmail/code', gmailCode);
+//////////
+// Server routes
+router.use('/gmail/code', gmailCodeRoute);
+router.use('/gmail/messages', messagesRoute);
 
 /**
  *
  */
-function gmailCode(req, res) {
+function gmailCodeRoute(req, res) {
+  if(!req.query.code) return res.send('Invalid request');
+
   let tok_path = path.resolve('keys', server.gmail_token);
 
   oAuth2Client.getToken(req.query.code, (err, token) => {
@@ -22,6 +28,60 @@ function gmailCode(req, res) {
       res.send('Token stored to: ' + tok_path);
     });
   });
+}
+
+/**
+ *
+*/
+function messagesRoute(req, res) {
+  let init_params = {
+    userId: 'me',
+    q: 'from:no-reply@alertsp.chase.com'
+  }
+  let getPage = function(params, result) {
+    gmail.users.messages.list(params)
+      .then(response => {
+        let nextPage = response.data.nextPageToken;
+        let msg_promises = [];
+        let messages = [];
+
+        // Get contents of all messages
+        response.data.messages.forEach(function(msg) {
+          msg_promises.push(
+             spending.getMessage(msg.id)
+               .then(msg_body => {
+                 messages.push(msg_body);
+               })
+               .catch(err => {
+                 res.send(err);
+               })
+          );
+        });
+
+        // Wait until list of promises are resolved
+        Promise.all(msg_promises).then(() => {
+          let all_messages  = result.concat(messages);
+
+          if(nextPage) {
+             let next_params = {
+               userId: 'me',
+               pageToken: nextPage,
+               q: 'from:no-reply@alertsp.chase.com Your Daily Account Summary Alert From Chase'
+             }
+
+             getPage(next_params, all_messages);
+          } else {
+            res.json(all_messages);
+          }
+        });
+      })
+      .catch(err => {
+        res.send(err);
+      });
+  }
+
+  // Get all pages of messages recursively
+  getPage(init_params, []);
 }
 
 module.exports = router;
--- src/spending.js
+++ src/spending.js
@@ -45,98 +45,32 @@ function init() {
   }
 }
 
-function listLabels() {
-  gmail.users.labels.list({
+/**
+ *
+ */
+function getMessage(messageId) {
+  let params = {
     userId: 'me',
-  }, (err, res) => {
-    if (err) return console.log('The API returned an error: ' + err);
-    const labels = res.data.labels;
+    id: messageId
+  }
 
-    //console.log(res.data);
-    if (labels.length) {
-      console.log('Labels:');
-      labels.forEach((label) => {
-        console.log(`- ${label.name}`);
+  return new Promise(function(resolve, reject) {
+    gmail.users.messages.get(params)
+      .then(res => {
+        let buff = new Buffer.from(res.data.payload.body.data, 'base64');
+        
+        resolve({
+          date: Date(res.data.internalDate),
+          body: buff.toString('ascii')
+        });
+      })
+      .catch(err => {
+        reject(err);
       });
-    } else {
-      console.log('No labels found.');
-    }
   });
 }
 
-
-
-/**
- * Get Message with given ID.
- *
- * @param  {String} userId User's email address. The special value 'me'
- * can be used to indicate the authenticated user.
- * @param  {String} messageId ID of Message to get.
- * @param  {Function} callback Function to call when the request is complete.
- */
-// function getMessage(auth, messageId) {
-//   const gmail = google.gmail({version: "v1", auth});
-
-//   gmail.users.messages.get({
-//     userId: "me",
-//     id: messageId,
-//     format: "raw"
-//   }, (err, res) => {
-//     if (err) return console.log("The API returned an error: " + err);
-
-//     let buff = new Buffer(res.data.raw, "base64");  
-//     let text = buff.toString("ascii");
-
-//     console.log(text);
-//   });
-// }
-
-// /**
-//  * Retrieve Messages in user's mailbox matching query.
-//  *
-//  * @param  {String} userId User's email address. The special value 'me'
-//  * can be used to indicate the authenticated user.
-//  * @param  {String} query String used to filter the Messages listed.
-//  * @param  {Function} callback Function to call when the request is complete.
-//  */
-// function listMessages(auth, callback) {
-//   const gmail = google.gmail({version: "v1", auth});
-
-//   gmail.users.messages.list({
-//     userId: "me"
-//   }, (err, res) => {
-//     if (err) return console.log("The API returned an error: " + err);
-
-//     callback(auth, res.data.messages[0].id);
-//   });
-// }
-
-// /**
-//  * Lists the labels in the user's account.
-//  *
-//  * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
-//  */
-// function listLabels(auth) {
-//   const gmail = google.gmail({version: 'v1', auth});
-
-//   gmail.users.labels.list({
-//     userId: 'me',
-//   }, (err, res) => {
-//     if (err) return console.log('The API returned an error: ' + err);
-//     const labels = res.data.labels;
-
-//     //console.log(res.data);
-//     if (labels.length) {
-//       console.log('Labels:');
-//       labels.forEach((label) => {
-//         console.log(`- ${label.name}`);
-//       });
-//     } else {
-//       console.log('No labels found.');
-//     }
-//   });
-// }
-
 module.exports = {
-  initialize: init
+  initialize: init,
+  getMessage: getMessage
 };
